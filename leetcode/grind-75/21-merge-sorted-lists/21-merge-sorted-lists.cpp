3f5b4fd9a483146f0d5fafe3342b9027c7a863fb
Bran <bran@MacBook-Pro.attlocal.net>
2022-07-27 15:38:59 -0500

    update leetcode 21

--- leetcode/grind-75/21-merge-sorted-lists/21-merge-sorted-lists.cpp
+++ leetcode/grind-75/21-merge-sorted-lists/21-merge-sorted-lists.cpp
@@ -11,6 +11,52 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // edge case -> garmanarnarr
+        if (!list1 && !list2) {
+            return list1;
+        } else if (!list1) {
+            return list2;
+        } else if (!list2) {
+            return list1;
+        }
+
+
+        // allocate a new node in the heap
+        ListNode* result = new ListNode { 0 };
+        ListNode* r = result;
         
+        // iterate through both lists
+        while (list1 && list2) {
+            // check if the nodes
+            if (list1->val <= list2->val) {
+                cout << list1->val << endl;
+                result->next = list1;
+                list1 = list1->next; 
+            } else if (list2->val <= list1->val) {
+                cout << list2->val << endl;
+                result->next = list2;
+                list2 = list2->next;
+            }
+            result = result->next;
+        }
+        
+        // add remaining nodes from the longer list
+        if (!list2) {
+            while (list1) {
+                result->next = list1;
+                list1 = list1->next;
+                result = result->next;
+            }
+        }
+        
+        if (!list1) {
+            while (list2) {
+                result->next = list2;
+                list2 = list2->next;
+                result = result->next;
+            }
+        }
+
+        return r->next;
     }
 };
